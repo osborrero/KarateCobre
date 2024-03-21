0ebe88ba6fc44bc7a13680bc09b68f3032e83c5a
osborrero <osborrero1992@gmail.com>
2024-03-21 15:34:29 -0500

    primer commit

---
+++ target/test-classes/karate-config.js
@@ -0,0 +1,13 @@
+function fn() {
+  var config = {
+    bucketName: 'test-automation-qa',
+    folderRecaudoFiles: 'files-to-cash-in',
+    fileExtension: '.csv',
+    baseUrl: 'https://test-container-qa.prueba.co/v1/entity/novelties/',
+    archivo_uno : 'recaudoTemplateCaracteresEspeciales.csv',
+    archivo_dos : 'recaudoTemplateDatosCorrectos.csv'
+
+  };
+
+  return config;
+}
